4bd15e3161f61289f2bff8ff6a259473aa9883e3
Alana <105035115+AlanaCalvo22@users.noreply.github.com>
2022-05-12 17:39:18 -0600

    CargarBDpaises

---
+++ CargarBDpaises.py
@@ -0,0 +1,18 @@
+import pickle
+def cargarBDpaíses(regionPais):
+    dicc={}
+    try:
+        archiRePa=open(regionPais, "r")
+        for i in archiRePa.readlines():
+            region=i.split(":")
+            pai=region[1].replace("\n"," ")
+            paises=pai.split(",")
+            dicc[region[0]]=paises
+        archiRePa.close()
+    
+    except:
+        print("No pudo abrir regionPais")
+    return(dicc)
+    
+print(cargarBDpaíses("regionPais.txt"))
+
